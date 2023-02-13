f1123ffb63fc6e5fa1e2e6ec5aaaa3f57c336a4e
Onlyllon1 <francescoliviero15@gmail.com>
2023-02-13 17:02:33 +0100

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Francesco on 13/02/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
