5c85da0b3b361d3778744d4c9d8ddc85b3e26373
Onlyllon1 <francescoliviero15@gmail.com>
2024-03-18 15:04:10 +0100

    Creazione programma nella gestione files C++

---
+++ Cerchio.cpp
@@ -0,0 +1,8 @@
+//
+//  Cerchio.cpp
+//  OOP
+//
+//  Created by Francesco on 21/02/23.
+//
+
+#include "Cerchio.hpp"
---
+++ Cerchio.hpp
@@ -0,0 +1,13 @@
+//
+//  Cerchio.hpp
+//  OOP
+//
+//  Created by Francesco on 21/02/23.
+//
+
+#ifndef Cerchio_hpp
+#define Cerchio_hpp
+
+#include <stdio.h>
+
+#endif /* Cerchio_hpp */
